5e7e9f1000b0e9df1b38834cd0eac6cb1d66b986
Joao Souza <joaoosouza07@gmail.com>
2024-03-16 20:47:54 -0300

    feat: add section gallery

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+var swiper = new Swiper(".swiperGallery", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
