b0110bb417beb3aab5f2262dc25a86d7b227101b
Joao Souza <joaoosouza07@gmail.com>
2024-03-17 20:53:31 -0300

    feat: menu sidebar

--- js/script.js
+++ js/script.js
@@ -22,3 +22,39 @@ var swiper = new Swiper(".swiperGallery", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+/* --------- MENU ------------- */
+
+const sidebar = document.querySelector(".sidebar");
+const btnSidebar = document.querySelector(".btn-sidebar");
+const menu = document.querySelector(".menu-content");
+const menuItems = document.querySelectorAll(".submenu-item");
+const submenuTitles = document.querySelectorAll(".submenu .menu-title");
+
+function toggleMenu() {
+    sidebar.classList.toggle("sidebar-active");
+}
+
+document.onclick = (e) => {
+    if (!btnSidebar.contains(e.target) && !sidebar.contains(e.target)) {
+        sidebar.classList.remove("sidebar-active");
+    }
+};
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        menu.classList.add("submenu-active");
+        item.classList.add("show-submenu");
+        menuItems.forEach((item2, index2) => {
+            if (index !== index2) {
+                item2.classList.remove("show-submenu");
+            }
+        });
+    });
+});
+
+submenuTitles.forEach((title) => {
+    title.addEventListener("click", () => {
+        menu.classList.remove("submenu-active");
+    });
+});
