bcc4add5cfed5d0b7309bf0bfeacd313982e2b6a
Joao Souza <joaoosouza07@gmail.com>
2024-03-19 10:19:39 -0300

    feat: add header dropdown menu mobile

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+/************* SWIPER WEB STORIES *************/
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
 });
 
+/************* SWIPER GALLERY *************/
 var swiper = new Swiper(".swiperGallery", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -23,24 +30,44 @@ var swiper = new Swiper(".swiperGallery", {
     },
 });
 
-/* --------- MENU ------------- */
+/************* HEADER MENU *************/
 
+/************* SHOW MENU *************/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+    toggle.addEventListener("click", () => {
+        // Add show-menu class to nav menu
+        nav.classList.toggle("show-menu");
+
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle("show-icon");
+    });
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/************* SIDEBAR MENU VARS *************/
 const sidebar = document.querySelector(".sidebar");
 const btnSidebar = document.querySelector(".btn-sidebar");
 const menu = document.querySelector(".menu-content");
 const menuItems = document.querySelectorAll(".submenu-item");
 const submenuTitles = document.querySelectorAll(".submenu .menu-title");
 
+/************* SHOW SIDEBAR *************/
 function toggleMenu() {
     sidebar.classList.toggle("sidebar-active");
 }
 
+/************* CLOSE SIDEBAR *************/
 document.onclick = (e) => {
     if (!btnSidebar.contains(e.target) && !sidebar.contains(e.target)) {
         sidebar.classList.remove("sidebar-active");
     }
 };
 
+/************* SUBMENU SIDEBAR ITEMS *************/
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         menu.classList.add("submenu-active");
